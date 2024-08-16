989c526333a14057d1116ddc4af2d70fb4b93607
DucHung299 <hunghauhinh66@gmail.com>
2024-08-16 14:21:55 +0700

    first commit

---
+++ buoi1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo '<h1 style="color:red; text-align:center;">Adu Vjppro</h1>';
+    echo '<img src=""E:\xampp\htdocs\baitap\anh.jpg"" alt="" width="500" height="500">';
+    ?>
+</body>
+</html>
